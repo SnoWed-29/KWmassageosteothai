8f2c906d32f68ab9992c5252d08ab4ee2d8478d3
Haitham Dihaji <126724431+SnoWed-29@users.noreply.github.com>
2023-05-20 16:00:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+
+gsap.from('.info',{
+    duration:1 ,
+    x: '-100%',
+});
+gsap.from('.crd1',{
+    duration: 0.7 ,
+    y: '-200%',
+});gsap.from('.crd2',{
+    duration: 1 ,
+    delay: .2,
+    y: '-200%',
+});gsap.from('.crd3',{
+    duration: 1 ,
+    delay: .4,
+    y: '-200%',
+});
+// gsap.from('#mainPic' ,{
+//     duration: 2.2,
+//     rotation:'720',
+//     scale: 1,
+//     delay: .5,
+// })
+// gsap.from('.h2',{
+//     duration: 1,
+//     delay: 2,
+//     opacity: 0,
+//     stagger: .5,
+// });
+// gsap.from('.animation',{
+//     duration: .8,
+//     delay: .5,
+//     opacity: 0,
+//     stagger: .4,
+// });
